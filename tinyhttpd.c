6add3fe7dc084d4cfafd8984527f276bbcbba63c
ou <1093520047@qq.com>
2017-01-03 22:01:30 +0800

    ok

--- tinyhttpd.c
+++ tinyhttpd.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // 处理从套接字上监听到的一个HTTP请求
-void accept_request(int);
+void accept_request(void *);
 
 // 返回给客户端这是一个错误请求
 void bad_request(int);
@@ -34,27 +34,6 @@ int startup(u_short*);
 void unimplemented(int);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 int main(void)
 {
     int server_sock = -1;
@@ -83,3 +62,121 @@ int main(void)
 
     return(0);
 }
+
+
+int startup(u_short *port)
+{
+    int httpd = 0;
+    int on = 1;
+    struct sockaddr_in name;
+    
+    // 创建套接字
+    httpd = socket(AF_INET, SOCK_STREAM, 0);
+    if (httpd == -1)
+        error_die("socket");
+    memset(&name, 0, sizeof(name));
+    name.sin_family = AF_INET;
+    name.sin_port = htons(*port);
+    name.sin_addr.s_addr = htonl(INADDR_ANY); // INADDR_ANY: 通配地址
+    if ((setsockopt(httpd, SOL_SOCKET, 
+                SO_REUSEADDR, &
+                , sizeof(on))) < 0)
+    {
+        error_die("setsockopt failed");
+    }
+}
+
+
+void error_die(const char* sc)
+{
+    perror(sc);
+    exit(1);
+}
+
+
+void accept_request(void *arg)
+{
+    int client = (intptr_t)arg;
+    char buf[1024];
+    size_t numchars;
+    char method[255];
+    char url[255];
+    char path[512];
+    size_t i = 0, j = 0;
+    struct stat st;
+    int cgi = 0;
+
+    char* query_string = NULL;
+    
+    numchars = get_line(client, buf, sizeof(buf)); // 从与客户端连接的套接字中读一行到buf中
+
+    // 将buf中从开头到第一个空格为止的内容拷贝到method中，末尾添上‘/0’
+    while (!ISspace(buf[i]) && (i < sizeof(method) - 1))
+    {
+        method[i] = buf[i];
+        i++;
+    }
+    j = i;
+    method[i] = '\0';
+
+
+    if (strcasecmp(method, "GET") && strcasecmp(method, "POST"))
+    {
+        unimplemented(client);
+        return;
+    }
+    if (strcasecmp(method, "POST") == 0)
+        cgi = 1;
+    
+    
+    i = 0;
+    while (ISspace(buf[j]) && (j < numchars))
+        j++;
+    while (!ISspace(buf[j]) && (i < sizeof(url) - 1) && (j < numchars))
+    {
+        url[i] = buf[j];
+        i++;
+        j++;
+    }
+    url[i] = '\0';
+
+    if (strcasecmp(method, "GET") == 0)
+    {
+        query_string = url;
+        while ((*query_string != '?') && (*query_string != '\0'))
+            query_string++;
+        if (*query_string == '?')
+        {
+            cgi = 1;
+            *query_string = '\0';
+            query_string++;
+        }
+    }
+
+    sprintf(path, "htdocs%s", url);
+    if (path[strlen(path) - 1] == '/')
+        strcat(path, "index.html");
+    if (stat(path, &st) == -1)
+    {
+        while ((numchars > 0) && strcmp("\n", buf))
+            numchars = get_line(client, buf, sizeof(buf));
+        not_found(client);
+    }
+    else
+    {
+        if ((st.st_mode & S_IFMT) == S_IFDIR)
+            strcat(path, "/index.html");
+        if ((st.st_mode & S_IXUSR) ||
+                (st.st_mode & S_IXGRP) ||
+                (st.st_mode & S_IXOTH))
+            cgi = 1;
+        if (!cgi)
+            serve_file(client, path);
+        else
+            execute_cgi(client, path, method, query_string);
+        
+    }
+
+    close(client);
+}
+
