ccf74749bd886bd2929a29f6b0fd31c7f48dc9f4
ou <1093520047@qq.com>
2017-01-09 10:59:12 +0800

    new

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all: httpd client
-LIBS = -lpthread
+LIBS = -pthread
 httpd: httpd.c
 	gcc -g -W -Wall $(LIBS) -o $@ $<
 
--- httpd.c
+++ httpd.c
@@ -93,12 +93,6 @@ int startup(u_short *port)
 }
 
 
-void error_die(const char* sc)
-{
-    perror(sc);
-    exit(1);
-}
-
 
 void accept_request(void *arg)
 {
@@ -196,29 +190,7 @@ void accept_request(void *arg)
     close(client);
 }
 
-void not_found(int client)
-{
-    char buf[1024];
 
-    sprintf(buf, "HTTP/1.0 404 NOT FOUND\r\n");
-    send(client, buf, strlen(buf), 0);
-    sprintf(buf, SERVER_STRING);
-    send(client, buf, strlen(buf), 0);
-    sprintf(buf, "Content-Type: text/html\r\n");
-    send(client, buf, strlen(buf), 0);
-    sprintf(buf, "\r\n");
-    send(client, buf, strlen(buf), 0);
-    sprintf(buf, "<HTML><TITLE>Not Found</TITLE>\r\n");
-    send(client, buf, strlen(buf), 0);
-    sprintf(buf, "<BODY><P>The server could not fulfill\r\n");
-    send(client, buf, strlen(buf), 0);
-    sprintf(buf, "your request because the resource specified\r\n");
-    send(client, buf, strlen(buf), 0);
-    sprintf(buf, "is unavailable or nonexistent.\r\n");
-    send(client, buf, strlen(buf), 0);
-    sprintf(buf, "</BODY></HTML>\r\n");
-    send(client, buf, strlen(buf), 0);
-}
 
 void serve_file(int client, const char* filename)
 {
@@ -241,6 +213,7 @@ void serve_file(int client, const char* filename)
     }
     fclose(resource);
 }
+
 
 void headers(int client, const char* filename)
 {
@@ -439,7 +412,29 @@ void unimplemented(int client)
 {
     char buf[1024];
 
-    sprintf(buf, "HTTP/1.0 500 NOT FOUND\r\n");
+    sprintf(buf, "HTTP/1.0 501 NOT FOUND\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, SERVER_STRING);
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "Content-Type: text/html\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "<HTML><HEAD><TITLE>Method Not Implemented\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "</TITLE></HEAD>\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "<BODY><P>HTTP request method not supported.\r\n");
+    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "</BODY><HTML>\r\n");
+    send(client, buf, strlen(buf), 0);
+}
+
+void not_found(int client)
+{
+    char buf[1024];
+
+    sprintf(buf, "HTTP/1.0 404 NOT FOUND\r\n");
     send(client, buf, strlen(buf), 0);
     sprintf(buf, SERVER_STRING);
     send(client, buf, strlen(buf), 0);
@@ -457,4 +452,12 @@ void unimplemented(int client)
     send(client, buf, strlen(buf), 0);
     sprintf(buf, "</BODY></HTML>\r\n");
     send(client, buf, strlen(buf), 0);
+}
+
+
+
+void error_die(const char* sc)
+{
+    perror(sc);
+    exit(1);
 }
